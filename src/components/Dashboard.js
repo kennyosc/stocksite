6ae4b2c86b8eb6589cd636dccc6faffa263c24a6
Kenny Oscar <kennyoscar95@gmail.com>
2020-06-01 15:19:50 +0700

    add: field for new inquiry

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -53,7 +53,86 @@ class Dashboard extends React.Component {
         } else {
             return (
                 <div className='container mt-5'>
-                    <h2 className='d-inline mr-4'>Dashboard</h2> <button className='btn btn-primary btn-sm mb-2'>+ Add Inquiry</button>
+                    <h2 className='d-inline mr-4'>Dashboard</h2>
+                    <button type="button" class="btn btn-primary btn-sm mb-2" data-toggle="modal" data-target="#exampleModal">+ Add Inquiry</button>
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Input New Inquiry</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputEmail1">Company Code</label>
+                                            <input onChange={this.handleChange} type="text" name='kodePerusahaan' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                            <small className="form-text text-muted">Please input your desired company code </small>
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Outstanding Stock</label>
+                                            <input onChange={this.handleChange} type="number" name='outstandingStock' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Companies outstanding stock (OS)</small>
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Equity</label>
+                                            <input onChange={this.handleChange} type="number" name='equity' className="form-control" id="exampleInputPassword1" />
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Liability</label>
+                                            <input onChange={this.handleChange} type="number" name='liability' className="form-control" id="exampleInputPassword1" />
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Net Profit</label>
+                                            <input onChange={this.handleChange} type="number" name='netProfit' className="form-control" id="exampleInputPassword1" />
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Stock Price</label>
+                                            <input onChange={this.handleChange} type="number" name='price' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Companies stock price at current date</small>
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Financial Date</label>
+                                            <input onChange={this.handleChange} type="date" name='financialDate' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Published financial statement date</small>
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Inflation</label>
+                                            <input onChange={this.handleChange} type="number" name='inflation' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Default to 50% per year</small>
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">Compound Annual Growth Rate (CAGR)</label>
+                                            <input onChange={this.handleChange} type="number" name='cagr' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Default to 8% per year</small>
+
+                                        </div>
+                                        <div className="form-group col-auto">
+                                            <label for="exampleInputPassword1">% Margin Of Safety</label>
+                                            <input onChange={this.handleChange} type="number" name='percentageMos' className="form-control" id="exampleInputPassword1" />
+                                            <small className="form-text text-muted">Your desired margin of safety. Defaults to 30%</small>
+
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Submit New Inquiry</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className='mt-4'>
                         <table className="table table-hover">
                             <thead>
